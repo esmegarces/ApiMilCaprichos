6023c5a8790ee784c226ca1d0ee276fee6232f95
Esmeralda Garces <jmgarces810@gmail.com>
2024-11-04 16:29:19 -0600

    Configuración de rutas agregadas

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\DetallesPostresController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route prueba
+Route::get('/saludo', function () {
+    return response()->json([
+        'Mensaje' => 'Saludo',
+        'nombre' => 'Esmeralda'
+    ], 200);
+});
+
+// Route DetallesPostre
+Route::get('/detalles_postres', [DetallesPostresController::class,'mostrar']);
+
